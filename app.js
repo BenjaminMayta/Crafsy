4884c53f3248501f1c7f3a63fbd17554900b4619
BenjaminMayta <maytabenjamin06@gmail.com>
2022-08-26 06:24:02 -0300

    Maquetado finalizado del home

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require ('path')
+const app = express()
+const port = 3015
+
+app.use(express.static(path.resolve(__dirname,'public')))
+
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','index.html')))
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')))
+
+app.listen(port,() => console.log(`Se levanto el servidor en http://localhost:${port} `))
